d3bf3182e822c1fb6cac4d2d5a23647d7516f9f5
unknown <checong@2652468632@qq.com>
2020-12-03 23:59:32 +0800

    删除字符串首尾字符

---
+++ CodeWars/字符串/第八级/删除首尾字符串/test.js
@@ -0,0 +1,15 @@
+/**
+ * It's pretty straightforward. 
+ * Your goal is to create a function that removes the first and last characters of a string. 
+ * You're given one parameter, the original string.
+ *  You don't have to worry with strings with less than two characters. 
+ */
+//删除首尾字符串
+function removeChar(str) {
+    let strArr = str.split('');
+    strArr.splice(0, 1);
+    strArr.splice(strArr.length - 1, 1);
+    return strArr.join('')
+};
+console.log(this.removeChar('woshinidie'));
+
